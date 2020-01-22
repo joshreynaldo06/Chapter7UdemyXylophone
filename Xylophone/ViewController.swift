c3bcdf75c830a311a9bd025646deb262e91dafc6
Joshua Ronaldo <joshuaronaldo088@gmail.com>
2020-01-22 12:16:39 +0700

    finished the sound with some additional animation

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,18 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var musicPlayer : AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    
+    
     @IBAction func cKeyPressed(_ sender: UIButton) {
-        print("I got pressed")
+        
+        
+        playSound(sender: "\(sender.currentTitle!)")
+        sender.alpha = 1
+        
     }
     
-
+    
+    @IBAction func keyPressedAlpha(_ sender: UIButton) {
+        sender.alpha = 0.5
+    }
+    
+    
+    func playSound(sender : String){
+        guard let url = Bundle.main.url(forResource: sender, withExtension: "wav") else{
+            return print("Wrong extension file")
+        }
+        musicPlayer = try! AVAudioPlayer(contentsOf: url)
+        musicPlayer?.play()
+        
+       
+    }
+    
 }
 
